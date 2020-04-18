429dc0dede37e56ab1c7eb51db8ad6bcdaf98da1
mobone <nicholas.reichel@gmail.com>
2020-04-18 14:14:51 -0500

    Update start_workers.py

--- start_workers.py
+++ start_workers.py
@@ -8,4 +8,5 @@ cores = multiprocessing.cpu_count()
 for i in range(int(cores)):
     #os.system('rq worker --url redis://192.168.1.127 &')
     #subprocess.Popen(['rq','worker','--url','redis://192.168.1.127', '>', '/tmp/worker.log', '2>&1'])
-    subprocess.call(['rq worker --url redis://192.168.1.127 > /tmp/worker.log 2>&1 &'])
+    #> /tmp/worker.log 2>&1 
+    subprocess.call(['rq worker --url redis://192.168.1.127 &'])
