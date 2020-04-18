933e0911b57787a1f68aec66789d958cdfc6fd11
mobone <nicholas.reichel@gmail.com>
2020-04-18 13:27:19 -0500

    Update start_workers.py

--- start_workers.py
+++ start_workers.py
@@ -2,7 +2,8 @@ import os
 import multiprocessing
 
 os.system('pkill -9 rq')
-os.system('cd /home/nick/multi_machine/')
+os.chdir('/home/nick/multi_machine')
+print(os.getwd())
 cores = multiprocessing.cpu_count()
 for i in range(int(cores)):
     os.system('rq worker --url redis://192.168.1.127 &')
