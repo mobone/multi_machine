f52a921970618a828140d6579874adc1bdd07453
mobone <nicholas.reichel@gmail.com>
2020-04-18 13:40:13 -0500

    Update start_workers.py

--- start_workers.py
+++ start_workers.py
@@ -1,9 +1,10 @@
 import os
 import multiprocessing
-
+import subprocess
 
 os.chdir('/home/nick/multi_machine')
 print(os.getcwd())
 cores = multiprocessing.cpu_count()
 for i in range(int(cores)):
-    os.system('rq worker --url redis://192.168.1.127 &')
+    #os.system('rq worker --url redis://192.168.1.127 &')
+    subprocess.run(['rq','worker','--url','redis://192.168.1.127','&'])
