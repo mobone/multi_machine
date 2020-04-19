71b0e2d99be5b5ac8ca6cb8736e3abb78383e7c7
mobone <nicholas.reichel@gmail.com>
2020-04-18 22:13:04 -0500

    working on start workers script

--- redis_runner.py
+++ redis_runner.py
@@ -41,6 +41,8 @@ class run_machine():
         self.with_rfc = with_rfc
         self.include_covid = include_covid
 
+        self.failed = False
+
         self.features = ['return', starting_feature]
         self.conn = sqlite3.connect('results.db')
 
@@ -70,104 +72,117 @@ class run_machine():
             self.create_jobs()
             self.get_redis_results()
 
+            if self.failed == True:
+                break
+
 
     def create_jobs(self):
-        q = Queue(connection=Redis( host='192.168.1.127' ))
-        for new_feature in self.feature_choices:
-            if new_feature in self.features:
-                continue
+        with Redis( host='192.168.1.127' ) as redis_con:
+            q = Queue(connection=redis_con)
+            for new_feature in self.feature_choices:
+                if new_feature in self.features:
+                    continue
 
-            test_features = self.features + [new_feature]
-            #print(self.params, test_features)
+                test_features = self.features + [new_feature]
+                #print(self.params, test_features)
 
-            feature_hash = str(self.params)+'_'+str(test_features)
-            #print(feature_hash)
-            
-            feature_hash = hashlib.md5(feature_hash.encode()).hexdigest()
-
-            previous_result = self.lookup_result(feature_hash)
-
-            if previous_result is None:
-                name = namegenerator.gen()
-
-                job_name = name+'__'+str(test_features)+'__'+feature_hash
+                feature_hash = str(self.params)+'_'+str(test_features)
+                #print(feature_hash)
                 
-                print('creating job', job_name)
-                # TODO: possibly use different queues for each simulation run
-                job_args = (test_features, 
-                            self.n_subsets, 
-                            self.n_components, 
-                            self.lookback, 
-                            self.with_rfc, 
-                            self.include_covid,
-                            name, )
-                job = q.enqueue(generate_model, args = job_args, job_timeout='6h',  result_ttl=3600 )
-                #TODO: change to nan and check for isnan in redis results
-                self.results = self.results.append( [ [str(test_features),  str(self.params), feature_hash, job.id, np.inf, np.inf, job.get_status()] ] )
-                self.jobs.append( (job.id, job_name) )
-            else:
-                #print(previous_result)
-                sharpe_ratio = float(previous_result['sharpe_ratio'])
-                cum_returns = float(previous_result['cum_returns'])
-                #print('found result', job_name, sharpe_ratio, cum_returns)
-                self.results = self.results.append( [ [str(test_features), str(self.params), feature_hash, 'previous', sharpe_ratio, cum_returns, 'previous'] ] )
-        self.results.columns = ['features', 'params', 'feature_hash', 'job_id', 'sharpe_ratio', 'cum_returns', 'job_status']
-        self.results = self.results.reset_index(drop=True)
+                feature_hash = hashlib.md5(feature_hash.encode()).hexdigest()
+
+                previous_result = self.lookup_result(feature_hash)
+
+                if previous_result is None:
+                    name = namegenerator.gen()
+
+                    job_name = name+'__'+str(test_features)+'__'+feature_hash
+                    
+                    print('creating job', job_name)
+                    # TODO: possibly use different queues for each simulation run
+                    job_args = (test_features, 
+                                self.n_subsets, 
+                                self.n_components, 
+                                self.lookback, 
+                                self.with_rfc, 
+                                self.include_covid,
+                                name, )
+                    job = q.enqueue(generate_model, args = job_args, job_timeout='6h',  result_ttl=3600 )
+                    #TODO: change to nan and check for isnan in redis results
+                    self.results = self.results.append( [ [str(test_features),  str(self.params), feature_hash, job.id, np.inf, np.inf, job.get_status()] ] )
+                    self.jobs.append( (job.id, job_name) )
+                else:
+                    #print(previous_result)
+                    sharpe_ratio = float(previous_result['sharpe_ratio'])
+                    cum_returns = float(previous_result['cum_returns'])
+                    #print('found result', job_name, sharpe_ratio, cum_returns)
+                    self.results = self.results.append( [ [str(test_features), str(self.params), feature_hash, 'previous', sharpe_ratio, cum_returns, 'previous'] ] )
+            self.results.columns = ['features', 'params', 'feature_hash', 'job_id', 'sharpe_ratio', 'cum_returns', 'job_status']
+            self.results = self.results.reset_index(drop=True)
 
 
     def get_redis_results(self):
         best_sharpe_ratio = -np.inf
         while True:
-            redis_con = Redis( host='192.168.1.127' )
-            #q = Queue(connection=redis_con)
-            #registry = FinishedJobRegistry(queue=q)
-            for job_id, job_name in self.jobs:
-                name, features, feature_hash = job_name.split('__')
-                job = Job.fetch(job_id, connection = redis_con)
+            try:
+                with Redis( host='192.168.1.127' ) as redis_con:
+                    #q = Queue(connection=redis_con)
+                    #registry = FinishedJobRegistry(queue=q)
+                    for job_id, job_name in self.jobs:
+                        name, features, feature_hash = job_name.split('__')
+                        job = Job.fetch(job_id, connection = redis_con)
 
-                job_status = job.get_status()
-                self.results.loc[self.results['features']==str(features), 'job_status'] = job_status
-                #print(job_id, job_status, type(job_status))
-                if job_status == 'started' or job_status == 'queued':
-                    continue
-                
-                # if backtest has already been completed, continue
-                if self.results[self.results['feature_hash']==feature_hash]['sharpe_ratio'].values[0] != np.inf:
-                    continue
-                try:
-                    test_with_states, models_used, num_models_used = job.result
-                except Exception as e:
-                    print(e)
-                    continue
-                print('running backtest')
-                backtest_results = get_backtest(test_with_states, feature_hash, features, self.params, models_used, num_models_used, name=name, show_plot=False)
-                print(backtest_results)
-                sharpe_ratio = float(backtest_results['sharpe_ratio'])
-                cum_returns = float(backtest_results['cum_returns'])
+                        job_status = job.get_status()
+                        self.results.loc[self.results['features']==str(features), 'job_status'] = job_status
+                        #print(job_id, job_status, type(job_status))
+                        if job_status == 'started' or job_status == 'queued':
+                            continue
+                        
+                        # if backtest has already been completed, continue
+                        if self.results[self.results['feature_hash']==feature_hash]['sharpe_ratio'].values[0] != np.inf:
+                            continue
+                        try:
+                            test_with_states, models_used, num_models_used = job.result
+                        except Exception as e:
+                            print(e)
+                            continue
+                        print('running backtest')
+                        backtest_results = get_backtest(test_with_states, feature_hash, features, self.params, models_used, num_models_used, name=name, show_plot=False)
+                        print(backtest_results)
+                        sharpe_ratio = float(backtest_results['sharpe_ratio'])
+                        cum_returns = float(backtest_results['cum_returns'])
 
-                self.results.loc[self.results['features']==str(features), 'sharpe_ratio'] = sharpe_ratio
-                self.results.loc[self.results['features']==str(features), 'cum_returns'] = cum_returns
-                #self.results.loc[self.results['features']==str(features), 'job_status'] = job_status
-                
-                best_features = self.results[ self.results['sharpe_ratio'] == self.results['sharpe_ratio'].max() ]['features'].values[0]
-                # remove job
-                #if job_status == 'finished':
-                #    registry.remove(job_id)
-                # todo: store failed jobs
-                if 'win_rate' in backtest_results.columns:
-                    backtest_results.to_sql('results', self.conn, if_exists='append')
-                if sharpe_ratio > best_sharpe_ratio:
-                    if sharpe_ratio > 1:
-                        plot(test_with_states, name=name, show=False)
-                    best_sharpe_ratio = sharpe_ratio
-                    best_features = features
+                        self.results.loc[self.results['features']==str(features), 'sharpe_ratio'] = sharpe_ratio
+                        self.results.loc[self.results['features']==str(features), 'cum_returns'] = cum_returns
+                        #self.results.loc[self.results['features']==str(features), 'job_status'] = job_status
+                        
+                        best_features = self.results[ self.results['sharpe_ratio'] == self.results['sharpe_ratio'].max() ]['features'].values[0]
+                        # remove job
+                        #if job_status == 'finished':
+                        #    registry.remove(job_id)
+                        # todo: store failed jobs
+                        if 'win_rate' in backtest_results.columns:
+                            backtest_results.to_sql('results', self.conn, if_exists='append')
+                        if sharpe_ratio > best_sharpe_ratio:
+                            if sharpe_ratio > 1:
+                                plot(test_with_states, name=name, show=False)
+                            best_sharpe_ratio = sharpe_ratio
+                            best_features = features
+            except Exception as e:
+                print(e)
+                sleep(5)
+                continue
             
             print(self.results)
             num_queued = len(self.results[self.results['job_status']=='queued'])
             num_started = len(self.results[self.results['job_status']=='started'])
             if (num_queued + num_started)>0:
                 print('waiting for', num_queued, num_started)
-                sleep(10)
+                sleep(30)
+
+            
+            
+
             else:
                 break
             
@@ -175,10 +190,15 @@ class run_machine():
         print('found best features', best_features)
         self.features = eval(best_features)
 
+        # sharpe ratio not good enough, just quit
+        if best_sharpe_ratio<.75:
+            self.failed = True
+
 
 
 def runner_method(params_with_features):
     params, feature_choices = params_with_features
+
     run_machine(params, feature_choices)
 
 
--- start_workers.sh
+++ start_workers.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 for run in {1..24}
 do
     rq worker --url redis://192.168.1.127 &
